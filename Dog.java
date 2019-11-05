fc009e1099193ca7dbd3a0d577b638e9161b8504
jadeb99 <55840691+jadeb99@users.noreply.github.com>
2019-11-05 12:51:03 +0000

    Dog.java

---
+++ Dog.java
@@ -0,0 +1,44 @@
+//java classes and objects
+
+/*think of a class as a template/blueprint that describes
+the behaviour and state of an objects
+*/
+
+/*An object is an insatnce of a class. Objects
+have states and behaviour
+
+state: some attributes of the object (colour breed age etc.)
+behaviour: methods and variables associated with the objects
+
+*/
+
+public class Dog {
+	public static void main (String[]args){
+	
+		//define attributes
+		String breed;
+		int age;
+		String colour;
+	
+		//the guys above are called instance variables (like global variables)
+		//there are 3 types of variables
+		//Static (class), Instance, Local
+		//Class variable changes can be seen across every object
+	}
+	
+	//declaring method barking
+	void barking() {
+		
+	}
+	
+	//declaring method hungry
+	void hungry() {
+		
+	}
+	
+	//declaring method slepping
+	void sleeping() {
+		
+	}
+	
+}
